79e8316a6cd94384cefb60719a5d13d5ef8ade57
zixiang930 <zixiangzhao930@gmail.com>
2026-08-12 16:35:49 +0800

    Fix Ruff import ordering

--- custom_components/ha_local_memory/config_flow.py
+++ custom_components/ha_local_memory/config_flow.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 from typing import Any
 
 import voluptuous as vol
-
 from homeassistant import config_entries
 from homeassistant.data_entry_flow import FlowResult
 
